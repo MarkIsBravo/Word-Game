6fddf9ac4c85c7b4c97dbaa04667d2a4741cf44c
Mark Ma <mazheng1022@hotmail.com>
2017-09-14 13:54:55 -0400

    add migration and seed

---
+++ db/migrations/migration-091417.sql
@@ -0,0 +1,47 @@
+\c word_game_dev;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_digest TEXT NOT NULL,
+    email VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS words (
+    id SERIAL PRIMARY KEY,
+    spell VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users_words (
+    id SERIAL PRIMARY KEY,
+    spell VARCHAR(255),
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    img_url VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS users_characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    img_url VARCHAR(255),
+    user_id INTEGER REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS inventory (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    usage VARCHAR(255),
+    price INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS users_inventory (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    usage VARCHAR(255),
+    price INTEGER,
+    user_id INTEGER REFERENCES users(id)
+);
