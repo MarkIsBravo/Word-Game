95c4e367a7e09ae5025e05ee1f690af6566ee04c
Mark Ma <mazheng1022@hotmail.com>
2017-09-15 01:09:14 -0400

    user delete and edit backend

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -9,6 +9,7 @@ usersController.create = (req,res) => {
     User.create({
         username: req.body.username,
         password_digest: hash,
+        nickname: req.body.nickname,
         email: req.body.email,
     })
     .then(user => {
@@ -25,13 +26,39 @@ usersController.create = (req,res) => {
         console.log(err);
         res.status(500).json(err);
     });
-}
+};
 
 usersController.index = (req,res) => {
     res.json({
         user: req.user,
         data: 'Put a user profile on this route'
     });
-}
+};
+//edit user's info
+usersController.update = (req, res) => {
+    console.log(req.params);
+    User.update(req.body.nickname, req.body.email, req.params.id)
+    .then(user => {
+        res.json(user);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+};
+//delete user
+usersController.delete = (req, res) => {
+    User.destroy(req.params.id)
+    .then(user => {
+        res.json({
+            message: 'ok',
+            user: user,
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+};
 
 module.exports=usersController;
--- models/user.js
+++ models/user.js
@@ -11,10 +11,29 @@ User.findByUserName = username => {
 User.create = user => {
     return db.one(`
     INSERT INTO users
-    (username, password_digest, email)
-    VALUES ($1, $2, $3)
+    (username, password_digest, nickname, email)
+    VALUES ($1, $2, $3, $4)
     RETURNING *
-    `,[user.username, user.password_digest, user.email]);
+    `,[user.username, user.password_digest, user.nickname, user.email]);
+};
+//edit users info
+User.update = (nickname, email, id) => {
+    return db.one(`
+    UPDATE users SET
+    nickname = $1,
+    email = $2
+    WHERE id = $3
+    RETURNING *
+    `, [nickname, email, id]);
+};
+//delete users
+User.destroy = (id, user_id) => {
+    return db.none(`
+    DELETE FROM users_words where user_id = $1;
+    DELETE FROM users_characters where user_id = $1;
+    DELETE FROM users_inventory where user_id = $1;
+    DELETE FROM users where id = $1
+    `, [id]);
 };
 
 module.exports = User;
